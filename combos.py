66d25dfa886d2a0871d97d8a6264e689d28eaae5
Henrique Melo <108189766+HenryMelo23@users.noreply.github.com>
2025-05-03 21:33:29 -0300

    Atualização automática do combos.py via interface

--- combos.py
+++ combos.py
@@ -1,1 +1,25 @@
-[]
+combos = [
+  {
+    "combo": [
+      {
+        "titulo": "Dom Casmurro",
+        "autor": "Machado de Assis",
+        "preco_original": "R$17,25",
+        "preco": "R$13,80"
+      },
+      {
+        "titulo": "1984",
+        "autor": "George Orwell",
+        "preco_original": "R$20,38",
+        "preco": "R$16,30"
+      },
+      {
+        "titulo": "A teoria de Tudo",
+        "autor": "Jane Hawking",
+        "preco_original": "R$15,26",
+        "preco": "R$12,21"
+      }
+    ],
+    "desconto": 20
+  }
+]
